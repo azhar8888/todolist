27379f4769a686ba0dfb834160bc1421e57cfa0f
shaikh Azharuddin <112998169+azhar8888@users.noreply.github.com>
2023-04-07 08:35:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {  useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'inc':
+      return { count: state.count + 1 }
+    case 'dec':
+      return { count: state.count - 1 }
+  }
+
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+
+  function inc() {
+    dispatch({ type: 'inc' })
+  }
+  function dec() {
+    dispatch({ type: 'dec' })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={dec}>-</button>
+        <span>{state.count}</span>
+        <button onClick={inc}>+</button>
+
       </header>
     </div>
   );
